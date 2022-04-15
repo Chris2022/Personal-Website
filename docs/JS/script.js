0048816b9e6cac9ca2189e71c4be38190a076129
Chris2022 <ctejada415gmail.com>
2022-04-15 10:31:27 -0400

    working with media rules

--- docs/JS/script.js
+++ docs/JS/script.js
@@ -1,12 +1,32 @@
 // This is JS code to make the navbar sticky
 let nav = document.querySelector(".navbar");
-window.onscroll = function(){
-    if(document.documentElement.scrollTop > 25){
-        nav.classList.add("sticky");
-        console.log("Activated");
-    }
-    else{
-        nav.classList.remove("sticky");
-        console.log("Unactivated");
+if (($(window).width() > 600)){
+    console.log($(window).width());
+    console.log("hello");
+    window.onscroll = function () {
+        if (document.documentElement.scrollTop > 25) {
+            nav.classList.add("sticky");
+            console.log("Activated");
+        }
+        else {
+            nav.classList.remove("sticky");
+            console.log("Unactivated");
+        }
     }
 }
+else{
+    nav.classList.remove("sticky");
+    console.log("Sticky removed");
+}
+
+// let nav = document.querySelector(".navbar");
+// window.onscroll = function(){
+//     if(document.documentElement.scrollTop > 25){
+//         nav.classList.add("sticky");
+//         console.log("Activated");
+//     }
+//     else{
+//         nav.classList.remove("sticky");
+//         console.log("Unactivated");
+//     }
+// }
