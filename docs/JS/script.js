14af7cd0b4a9e09d1f60d77f304b5f3abaf0cf0b
Chris2022 <ctejada415gmail.com>
2022-04-24 21:20:34 -0400

    working with responsiveness of webpage

--- docs/JS/script.js
+++ docs/JS/script.js
@@ -18,15 +18,3 @@ else{
     nav.classList.remove("sticky");
     console.log("Sticky removed");
 }
-
-// let nav = document.querySelector(".navbar");
-// window.onscroll = function(){
-//     if(document.documentElement.scrollTop > 25){
-//         nav.classList.add("sticky");
-//         console.log("Activated");
-//     }
-//     else{
-//         nav.classList.remove("sticky");
-//         console.log("Unactivated");
-//     }
-// }
