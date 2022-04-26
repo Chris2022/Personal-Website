3a1215e3d0cd817120af42e8c630cf1a5fb8cd54
Chris2022 <ctejada415gmail.com>
2022-04-26 10:41:47 -0400

    media rule updates, page updates, css updates, JS modifications

--- docs/JS/script.js
+++ docs/JS/script.js
@@ -1,20 +1,31 @@
 // This is JS code to make the navbar sticky
 let nav = document.querySelector(".navbar");
+let active = document.querySelector(".active");
 if (($(window).width() > 600)){
-    console.log($(window).width());
-    console.log("hello");
     window.onscroll = function () {
         if (document.documentElement.scrollTop > 25) {
+            //add the sticky class to the navbar when you reach its scroll position
             nav.classList.add("sticky");
-            console.log("Activated");
+            // remove the active tabs as it does not match the sticky navbar
+            active.classList.remove("active");
         }
         else {
             nav.classList.remove("sticky");
-            console.log("Unactivated");
         }
     }
 }
 else{
     nav.classList.remove("sticky");
-    console.log("Sticky removed");
+}
+
+let logo = document.querySelector(".logo");
+if(($(window).width() < 600)){
+    window.onscroll = function () {
+        if (document.documentElement.scrollTop > 20) {
+            logo.classList.add("hide");
+        }
+        else{
+            logo.classList.remove("hide");
+        }
+    }
 }
